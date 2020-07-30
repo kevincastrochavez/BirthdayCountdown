33897d96588403e2d8c8d503fd6f2679c74d003c
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-29 20:51:48 -0500

    modified the birthday form

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -7,15 +7,24 @@ class BirthdayForm extends Component {
         super(props);
 
         this.state = {
-            startDate: moment()
+            startDate: moment(),
+            formCompleted: false
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleGenerate = this.handleGenerate.bind(this);
+        this.renderStuff = this.renderStuff.bind(this);
     }
 
     handleChange(date) {
         this.setState({
             startDate: date
+        })
+    }
+
+    handleGenerate() {
+        this.setState({
+            formCompleted: true
         })
     }
     
@@ -30,6 +39,8 @@ class BirthdayForm extends Component {
                     selected={this.state.startDate}
                     onChange={this.handleChange}
                 />
+
+                <a onClick={this.handleGenerate}>Generate Countdown</a>
             </div>
         );
     };
