fa49ba0c10850a9cea4468aca783aec05734168a
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-29 22:03:11 -0500

    added time calculation

--- src/components/clock.js
+++ src/components/clock.js
@@ -6,7 +6,28 @@ class Clock extends Component {
         super();
 
         this.state = {
-            timeRemaining: {}
+            timeRemaining: this.getTimeRemaining(this.props.birthdayFormState.startDate.toString())
+        }
+
+        this.getTimeRemaining = this.getTimeRemaining.bind(this);
+    }
+
+    getTimeRemaining(birthday) {
+        var bday = new Date(birthday);
+        let today = new Date();
+
+        var distance = bday.getTime() - today.getTime();
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        return {
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
         }
     }
     
@@ -21,10 +42,6 @@ class Clock extends Component {
                 </div>
             );
     };
-
-    componentDidMount() {
-        this.birthday = this.props.birthdayFormState.startDate.toString();
-    }
 }
 
 export default Clock;
