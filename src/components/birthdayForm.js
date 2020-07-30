50d4f2e16dfae345bfc9eb40b73ba9639cb1778b
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-29 20:24:34 -0500

    installed react datepicker, calendar and moment dependencies and implemented the datepicker component

---
+++ src/components/birthdayForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+
+class BirthdayForm extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>
+                    Birthday Form
+                </h1>
+
+                <DatePicker/>
+            </div>
+        );
+    };
+}
+
+export default BirthdayForm;
