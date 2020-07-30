a72eb95e3276b2fdd3162a2be1a583a27b58e46d
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-30 12:49:23 -0500

    fixed same day case

--- src/components/clock.js
+++ src/components/clock.js
@@ -33,6 +33,8 @@ class Clock extends Component {
                 bday.setFullYear(today.getFullYear());
             } else if (birthDay < currentDay) {
                 bday.setFullYear(today.getFullYear() + 1);
+            } else if (birthDay == currentDay) {
+                return 0;
             }
         }
        
@@ -51,7 +53,7 @@ class Clock extends Component {
         }
     }
 
-    getAge = functionr() {
+    getAge = function() {
         var bday = new Date(this.birthday);
         let today = new Date();
         
@@ -78,16 +80,23 @@ class Clock extends Component {
         
         return (
             <div>
-                <div>
-                    <div>DAYS {data.days}</div>
-                    <div>HRS {data.hours}</div>
-                    <div>MINS {data.minutes}</div>
-                    <div>SECS {data.seconds}</div>
-                </div>
+                {
+                    this.state.timeRemaining == 0 ? 
+                            <h1>Happy Birthday!</h1>
+                        :
+                        <div>
+                            <div>
+                                <div>DAYS {data.days}</div>
+                                <div>HRS {data.hours}</div>
+                                <div>MINS {data.minutes}</div>
+                                <div>SECS {data.seconds}</div>
+                            </div>
 
-                <div>
-                    {<h4>Remaining until you are {this.getAge()}</h4>}
-                </div>
+                            <div>
+                                {<h4>Remaining until you are {this.getAge()}</h4>}
+                            </div>
+                        </div>
+                }
             </div>
         );
     };
